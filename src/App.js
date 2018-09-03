92b19ec789054f3120aba90ef02c737f0cd0e781
seungmin <>
2018-09-03 13:51:30 +0900

    중간 저장

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyComponent from './MyComponent'
 
 class App extends Component {
   render() {
+
+    const text =  '당신은 어썸한가요';
+    const flag = true;
+    const style = {
+        backgroundColor: 'gray',
+        color: 'green'
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>리액트 안녕</h1>
+          <h2 style={ style }>{ text }</h2>
+          {
+              flag ? 'true' : 'false'
+          }
+          <br/>
+          {
+              flag && '보여주세요'
+          }
+          <MyComponent/>
       </div>
     );
   }
---
+++ src/MyComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component {
+
+    static defaultProps = {
+        name: 'seung min 2'
+    };
+
+    render() {
+        return (
+            <div>
+                Hello, My name is {this.props.name}
+            </div>
+        )
+    }
+
+};
+
+MyComponent.defaultProps = {
+    name: 'seung min'
+};
+
+MyComponent.propTypes= {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequird
+}
+
+export default MyComponent;
